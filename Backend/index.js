0a2cb00ec4e2c9ea74bc606e5a9110b2d765d4fa
Hantaro191702 <chaugiahan001@gmail.com>
2024-10-30 11:58:46 +0700

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,33 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
 
 const app = express();
 
-app.get("/", (_,res) => {
-    return res.status(200).json({
-        message:"I'm coming from backend",
-        success:true
-    })
-})
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => {
+  return res.status(200).json({
+    message: "I'm coming from backend",
+    success: true,
+  });
+});
 
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended:true}));
+app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credential:true
-}
+  origin: "http://localhost:5173",
+  credential: true,
+};
 app.use(cors(corsOptions));
 
-PORT = 8000;
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
