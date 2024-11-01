4ac7831c95bd9c3911a93c7ed06b96608eec17ce
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-01 19:47:53 +0700

    update Signup

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,28 +1,79 @@
 import { Label } from "./ui/label"
 import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+import { toast } from "sonner"
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
-  return (
-    <div className='flex items-center w-screen h-screen justify-center'>
-        <form actio className="shadow-lg flex flex-col gap-5 p-8">
-            <div>
-                <h1>LOGO</h1>
-                <p>Signup to see photos & videos from your friends</p>
-                
-            </div>
-            <div>
-                <span className="font-medium">Username</span>
-                <Input
-                    type="text"
-                    className="focus-visible:ring-transparent my-2"
-                  
-                />
-            </div>
-        </form>
-    </div>
-  )
+    const [input, setInput] = useState ({
+        username:"",
+        email:"",
+        password:""
+    });
+    const changeEventHandler = (e) => {
+        setInput({...input, [e.target.name]:e.target.value});
+    }
+
+    const signupHandler = async (e) => {
+        e.prevenDefault();
+        console.log(input);
+        try {
+            const res = await axios.post('http://localhost:8000/api/v1/user/register', input, {
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                withCredentials:true
+            });
+            if(res.data.success) {
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className='flex items-center w-screen h-screen justify-center'>
+            <form onSubmit={signupHandler} action="" className="shadow-lg flex flex-col gap-5 p-8">
+                <div>
+                    <h1>LOGO</h1>
+                    <p>Signup to see photos & videos from your friends</p>
+                    
+                </div>
+                <div>
+                    <span className="font-medium">Username</span>
+                    <Input
+                        type="text"
+                        name="username"
+                        value={input.username}
+                        onChange={changeEventHandler}
+                        className="focus-visible:ring-transparent my-2"     
+                    />
+                </div>
+                <div>
+                    <span className="font-medium">Email</span>
+                    <Input
+                        type="email"
+                        name="email"
+                        value={input.email}
+                        onChange={changeEventHandler}
+                        className="focus-visible:ring-transparent my-2"     
+                    />
+                </div>
+                <div>
+                    <span className="font-medium">Password</span>
+                    <Input
+                        type="password"
+                        name="password"
+                        value={input.password}
+                        onChange={changeEventHandler}
+                        className="focus-visible:ring-transparent my-2"     
+                    />
+                </div>
+                <Button type="submit">Signup</Button>
+            </form>
+        </div>
+    )
 }
 
 export default Signup
