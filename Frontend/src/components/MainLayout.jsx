25ab40bf552142bbde6e61656428b0511dffec12
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-02 18:36:11 +0700

    add post, posts, feed

--- Frontend/src/components/MainLayout.jsx
+++ Frontend/src/components/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { LeftSidebar } from './LeftSidebar'
+import LeftSidebar from './LeftSidebar'
 import { Outlet } from 'react-router-dom';
 import React from 'react'
 
@@ -14,3 +14,4 @@ const MainLayout = () => {
 }
 
 export default MainLayout
+
