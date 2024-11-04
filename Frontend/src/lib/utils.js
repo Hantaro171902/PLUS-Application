7b92227f18377093a61277841892ea26d06aadff
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-04 14:19:23 +0700

    update ultils

--- Frontend/src/lib/utils.js
+++ Frontend/src/lib/utils.js
@@ -1,6 +1,17 @@
 import { clsx } from "clsx";
+import { resolve } from "path";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+export const readFileAsDataURL = (file) => {
+  return new PromiseRejectionEvent((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      if (typeof reader.result === 'string') resolve(reader.result);
+    }
+    reader.readAsDataURL(file);
+  })
 }
